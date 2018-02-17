b777578e7ac5e58dc0255428645c9d4cac4ba920
Rafael Belfort <rbelfort88@yahoo.es>
2018-02-17 16:55:24 -0600

    First commit

---
+++ 1_Project/js/JavaScript.js
@@ -0,0 +1,9 @@
+/*
+ * Esto es un borrador de JavaScript.
+ *
+ * Escriba algo de JavaScript, después haga clic derecho o elija del menú Acción:
+ * 1. Ejecutar para evaluar el texto seleccionado (Ctrl+R),
+ * 2. Inspeccionar para mostrar el inspector de objetos con el resultado (Ctrl+I), o,
+ * 3. Mostrar para insertar el resultado en un comentario después de la selección. (Ctrl+L)
+ */
+
